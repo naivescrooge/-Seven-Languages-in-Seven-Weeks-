396ec1c5f01ec3d8bc47eb48d5ff218281609892
Naive__Scrooge <2677284500@qq.com>
2021-08-24 05:42:22 +0800

    formatted all and finished day2

--- Lua/day1/day1.lua
+++ Lua/day1/day1.lua
@@ -1,12 +1,12 @@
-function call_114_times(f)
-    ff = function (num)
-        t = num
-        for i = 1, 114, 1 do
-            t = f(t)
-        end
-    end
-    return ff
-end
+-- function call_114_times(f)
+--     ff = function (num)
+--         t = num
+--         for i = 1, 114, 1 do
+--             t = f(t)
+--         end
+--     end
+--     return ff
+-- end
 
 function square(num)
     return num * num
@@ -34,19 +34,19 @@ end
 -- print_abc(1, 11)
 -- print_abc(1, 11, 114)
 
--- print_args(1,1,4,5,1,4,'scrooge',true)
+-- print_args(1, 1, 4, 5, 1, 4, 'scrooge', true)
 
 function fac(num)
     if num == 0 then
         return 1
     end
-    return fac(num-1)*num
+    return fac(num - 1) * num
 end
 
 -- print(fac(20))
 
-function swap(a,b)
-    return b,a
+function swap(a, b)
+    return b, a
 end
 
 -- print(swap(3,4))
@@ -69,17 +69,17 @@ end
 -- end
 
 function test()
-    i=math.random(100)
+    i = math.random(100)
     while i < 100 do
         print(i)
-        i=math.random(100) --怎么随机序列是固定的？
+        i = math.random(100) --怎么随机序列是固定的？
     end
 end
 
 function hypotenuse(a,b)
-    local a2=a*a
-    local b2=b*b
-    return math.sqrt(a2+b2)
+    local a2 = a * a
+    local b2 = b * b
+    return math.sqrt(a2 + b2)
 end
 
 
@@ -103,7 +103,7 @@ function is_prime(num)
     elseif num == 2 then
         return true
     end
-    for i=3,math.sqrt(num),2 do
+    for i=3, math.sqrt(num), 2 do
         if num%i == 0 then
             return false
         end
@@ -111,45 +111,45 @@ function is_prime(num)
     return true
 end
 
--- print(is_prime(2),is_prime(3),is_prime(4),is_prime(5),is_prime(6),is_prime(7),is_prime(8),is_prime(9))
+-- print(is_prime(2), is_prime(3), is_prime(4), is_prime(5), is_prime(6), is_prime(7), is_prime(8), is_prime(9))
 
 function find(n)
-    local cnt=0
-    local i=-7
-    while cnt<n do
-        i=i+10
-        if is_prime(i)==true and ends_in_3(i)==true then
-            cnt=cnt+1
+    local cnt = 0
+    local i = -7
+    while cnt < n do
+        i = i + 10
+        if is_prime(i) == true and ends_in_3(i) == true then
+            cnt = cnt + 1
         end
     end
     return i
 end
 
--- print(find(1),find(2),find(3),find(4),find(5))
+-- print(find(1), find(2), find(3), find(4), find(5))
 
 function tt()
     print('yeah')
 end
 
-function for_loop(a,b,f)
-    local i=a
-    while i<=b do
+function for_loop(a, b, f)
+    local i = a
+    while i <= b do
         f()
-        i=i+1
+        i = i + 1
     end
 end
 
 -- for_loop(1,9,tt)
 
-function reduce(max,init,f)
-    for i=1,max,1 do
-        init=f(init,i)
+function reduce(max, init, f)
+    for i = 1, max, 1 do
+        init = f(init, i)
     end
     return init
 end
 
-function times(a,b)
-    return a*b
+function times(a, b)
+    return a * b
 end
 
 -- print(reduce(5,1,times))
--- Lua/day2/day2.lua
+++ Lua/day2/day2.lua
@@ -1,72 +1,72 @@
-book={
-    title="Grail Diary",
-    author="Henry Jones",
-    pages=100
+book = {
+    title = "Grail Diary", 
+    author = "Henry Jones", 
+    pages = 100
 }
 
 -- print(book.title)
 
--- book.stars=5
--- book.author="Henry Jones Sr."
--- key="stars"
+-- book.stars = 5
+-- book.author = "Henry Jones Sr."
+-- key = "stars"
 -- print(book[key])
 
--- book.pages=nil
+-- book.pages = nil
 -- print(book.pages)
 
 function print_table(t)
-    for k,v in pairs(t) do
+    for k, v in pairs(t) do
         print(k .. ": " .. v)
     end
 end
 
 -- print_table(book)
 
-medals={
-    "gold",
-    "silver",
+medals = {
+    "gold", 
+    "silver", 
     "bronze"
 }
 
--- print(medals[1],' ',medals[2],' ',medals[3],' ',medals[4])
--- medals[4]="lead"
--- print(medals[1],' ',medals[2],' ',medals[3],' ',medals[4])
+-- print(medals[1], ' ', medals[2], ' ', medals[3], ' ', medals[4])
+-- medals[4] = "lead"
+-- print(medals[1], ' ', medals[2], ' ', medals[3], ' ', medals[4])
 
-icecream_scoops={
-    "vanilla",
+icecream_scoops = {
+    "vanilla", 
     "chocolate";
-    sprinkles=true
+    sprinkles = true
 }
 
 -- print(icecream_scoops[1])
 -- print(icecream_scoops.sprinkles)
 
-greek_numbers={
-    ena="one",
-    dyo="two",
-    tria="three"
+greek_numbers = {
+    ena = "one", 
+    dyo = "two", 
+    tria = "three"
 }
 
 -- print(getmetatable(greek_numbers))
 
 -- function table2string(t)
---     local result={}
---     for k,v in pairs(t) do
---         result[#result+1] = k .. ": " .. v --#result 计算头元素到尾元素的长度 table.maxn(result) 返回数组的最大索引
+--     local result = {}
+--     for k, v in pairs(t) do
+--         result[#result + 1]  =  k .. ": " .. v --#result 计算头元素到尾元素的长度 table.maxn(result) 返回数组的最大索引
 --     end
---     return table.concat(result,"\n")
+--     return table.concat(result, "\n")
 -- end
 
--- mt={
---     __tostring=table2string
+-- mt = {
+--     __tostring = table2string
 -- }
 
--- setmetatable(greek_numbers,mt)
+-- setmetatable(greek_numbers, mt)
 -- -- print(greek_numbers)
 
--- local _private={}
+-- local _private = {}
 
--- function strict_read(table,key)
+-- function strict_read(table, key)
 --     if _private[key] then
 --         return _private[key]
 --     else
@@ -74,185 +74,313 @@ greek_numbers={
 --     end
 -- end
 
--- function strict_write(table,key,value)
+-- function strict_write(table, key, value)
 --     if _private[key] then
 --         error("Duplicate key: " .. key)
 --     else
---         _private[key]=value
+--         _private[key] = value
 --     end
 -- end
 
--- local mt={
---     __index=strict_read,
---     __newindex=strict_write
+-- local mt = {
+--     __index = strict_read, 
+--     __newindex = strict_write
 -- }
 
--- treasure={}
--- setmetatable(treasure,mt)
+-- treasure = {}
+-- setmetatable(treasure, mt)
 
--- treasure.gold=50
+-- treasure.gold = 50
 -- print(treasure.gold)
--- -- print(treasure.silver)
--- treasure.gold=100
+-- print(treasure.silver)
+-- treasure.gold = 100
 
--- dietrich={
---     name="Dietrich",
---     health=100,
+-- dietrich = {
+--     name = "Dietrich", 
+--     health = 100, 
 
---     take_hit=function (self)
---         self.health=self.health-10
+--     take_hit = function (self)
+--         self.health = self.health - 10
 --     end
 -- }
 
--- clone={
---     name=dietrich.name,
---     health=dietrich.health,
---     take_hit=dietrich.take_hit
+-- clone = {
+--     name = dietrich.name, 
+--     health = dietrich.health, 
+--     take_hit = dietrich.take_hit
 -- }
 
 -- dietrich.take_hit(dietrich)
 -- print(dietrich.health)
 -- print(clone.health)
 
--- Villain={
---     health=100,
+-- Villain = {
+--     health = 100, 
 
---     new=function (self,name)
---         local obj={
---             name=name,
---             health=self.health
+--     new = function (self, name)
+--         local obj = {
+--             name = name, 
+--             health = self.health
 --         }
 
---         setmetatable(obj,self)
---         self.__index=self
+--         setmetatable(obj, self)
+--         self.__index = self
 
 --         return obj
---     end,
+--     end, 
 
---     take_hit=function (self)
---         self.health=self.health-10
+--     take_hit = function (self)
+--         self.health = self.health - 10
 --     end
 -- }
 
--- dietrich=Villain.new(Villain,"Dietrich")
+-- dietrich = Villain.new(Villain, "Dietrich")
 -- Villain.take_hit(dietrich)
 -- dietrich.take_hit(dietrich)
 -- print(dietrich.health)
 
--- SuperVillain=Villain.new(Villain)
+-- SuperVillain = Villain.new(Villain)
 
 -- function SuperVillain.take_hit(self)
---     self.health=self.health-5
+--     self.health = self.health - 5
 -- end
 
--- toht=SuperVillain.new(SuperVillain,"Toht")
+-- toht = SuperVillain.new(SuperVillain, "Toht")
 -- toht.take_hit(toht)
 -- print(toht.health)
 
--- Villain={health=100}
+-- Villain = {health = 100}
 
 -- function Villain:new(name)
---     local obj={
---         name=name,
---         health=self.health
+--     local obj = {
+--         name = name, 
+--         health = self.health
 --     }
 
---     setmetatable(obj,self)
---     self.__index=self
+--     setmetatable(obj, self)
+--     self.__index = self
 
 --     return obj
 -- end
 
 -- function Villain:take_hit()
---     self.health=self.health-10
+--     self.health = self.health - 10
 -- end
 
--- SuperVillain=Villain:new()
+-- SuperVillain = Villain:new()
 
 -- function SuperVillain:take_hit()
---     self.health=self.health-5
+--     self.health = self.health - 5
 -- end
 
--- dietrich=Villain:new("Dietrich")
+-- dietrich = Villain:new("Dietrich")
 -- dietrich:take_hit()
 -- print(dietrich.health)
 
--- toht=SuperVillain:new("Toht")
+-- toht = SuperVillain:new("Toht")
 -- toht:take_hit()
 -- print(toht.health)
 
 function fibonacci()
-    local m=1
-    local n=1
+    local m = 1
+    local n = 1
     
     while true do
         coroutine.yield(m)
-        m,n=n,m+n
+        m, n = n, m + n
     end
 end
 
--- generator=coroutine.create(fibonacci)
--- succeeded,value=coroutine.resume(generator)
+-- generator = coroutine.create(fibonacci)
+-- succeeded, value = coroutine.resume(generator)
 -- print(value)
--- succeeded,value=coroutine.resume(generator)
+-- succeeded, value = coroutine.resume(generator)
 -- print(value)
--- succeeded,value=coroutine.resume(generator)
+-- succeeded, value = coroutine.resume(generator)
 -- print(value)
--- succeeded,value=coroutine.resume(generator)
+-- succeeded, value = coroutine.resume(generator)
 -- print(value)
--- succeeded,value=coroutine.resume(generator)
+-- succeeded, value = coroutine.resume(generator)
 -- print(value)
--- succeeded,value=coroutine.resume(generator)
+-- succeeded, value = coroutine.resume(generator)
 -- print(value)
 
--- scheduler=require 'scheduler'
+-- scheduler = require 'scheduler'
 
 -- function punch()
---     for i=1,5 do
+--     for i = 1, 5 do
 --         print('punch ' .. i)
 --         scheduler.wait(1.0)
 --     end
 -- end
 
 -- function block()
---     for i=1,3 do
+--     for i = 1, 3 do
 --         print('block ' .. i)
 --         scheduler.wait(2.0)
 --     end
 -- end
 
--- scheduler.schedule(0.0,coroutine.create(punch))
--- scheduler.schedule(0.0,coroutine.create(block))
+-- scheduler.schedule(0.0, coroutine.create(punch))
+-- scheduler.schedule(0.0, coroutine.create(block))
 
 -- scheduler.run()
 
 
 -- Practice
 
--- function concatnate(a1,a2)
---     tmp={}
---     for i=1,#a1 do
---         tmp[#tmp+1] = a1[i]
---     end
---     for i=1,#a2 do
---         tmp[#tmp+1] = a2[i]
---     end
---     return tmp
--- end
+function concatnate(a1, a2)
+    tmp = {}
+    for i = 1, #a1 do
+        tmp[#tmp + 1]  =  a1[i]
+    end
+    for i = 1, #a2 do
+        tmp[#tmp + 1]  =  a2[i]
+    end
+    return tmp
+end
 
--- function printarray(a)
---     for i=1,#a do
---         io.write(a[i])
---         io.write(" ")
---     end
---     io.write("\n")  --输出到io
--- end
+function printarray(a)
+    for i = 1, #a do
+        io.write(a[i])
+        io.write(" ")
+    end
+    io.write("\n")  --输出到io
+end
 
--- a1={1,1,4,5,1,4}
--- a2={1,9,1,9,8,1,0}
--- a3=concatnate(a1,a2)
+a1 = {1, 1, 4, 5, 1, 4}
+a2 = {1, 9, 1, 9, 8, 1, 0}
+a3 = {}
 
 -- printarray(a1)
 -- printarray(a2)
 -- printarray(a3)
 
+
+-- local _private = {}
+
+-- function strict_read(table, key)
+--     if _private[key] then
+--         return _private[key]
+--     else
+--         error("Invalid key: " .. key)
+--     end
+-- end
+
+-- function strict_write(table, key, value)
+--     if _private[key] and value ~= nil then
+--         error("Duplicate key: " .. key)
+--     elseif _private[key] then
+--         _private[key] = nil
+--     else
+--         _private[key] = value
+--     end
+-- end
+
+-- local mt = {
+--     __index = strict_read, 
+--     __newindex = strict_write
+-- }
+
+-- treasure = {}
+-- setmetatable(treasure, mt)
+
+-- treasure.gold = 100
+-- print(treasure.gold)
+-- treasure.gold = nil
+-- treasure.gold = 50
+-- print(treasure.gold)
+
+local mt = {
+    __add = concatnate
+}
+
+setmetatable(a1, mt)
+a3 = a1 + a2
+-- printarray(a3)
+
+
+Queue = {
+    size = 0,
+    elements = {},
+    new = function (self)
+        if self == nil then
+            self = Queue
+        end
+        local obj = {
+            size = self.size,
+            elements = self.elements,
+        }
+        setmetatable(obj, self)
+        self.__index = self
+        return obj
+    end,
+    add = function (num, self)
+        if self == nil then
+            self = Queue
+        end
+        self.size = self.size + 1
+        self.elements[self.size] = num
+    end,
+    remove = function (self)
+        if self == nil then
+            self = Queue
+        end
+        if self.size == 0 then
+            return nil
+        else
+            self.size = self.size - 1
+            local ret = self.elements[1]
+            table.remove(self.elements, 1)
+            return ret
+        end
+    end
+}
+
+-- q = Queue.new()
+-- print(q.remove())
+-- q.add(20)
+-- q.add(30)
+-- print(q.remove())
+-- q.add(1)
+-- q.add(1)
+-- q.add(4)
+-- q.add(5)
+-- q.add(1)
+-- q.add(4)
+-- print(q.remove())
+-- print(q.remove())
+-- print(q.remove())
+-- print(q.remove())
+-- print(q.remove())
+-- print(q.remove())
+-- print(q.remove())
+-- print(q.remove())
+
+function retry(count, body)
+    generator = {coroutine.create(body)}
+    for i = 1, count, 1 do
+        succeeded, value = coroutine.resume(generator[i])
+        generator[#generator + 1] = coroutine.create(body) 
+        if type(value) == "nil" then
+            print("Retry Succeeded!\nOmedeto!")
+            return
+        else
+            print("The" .. i .. "-th retry failed, retrying......")
+        end
+    end
+    print("zannendeshita\nqwq")
+end
+
+-- math.randomseed(os.time())
+
+-- retry(10,
+--     function ()
+--         tmp = math.random()
+--         print(tmp)
+--         if tmp > 0.1 then
+--             coroutine.yield("Somethine bad happened")
+--         end
+--         print("Succeeded")
+--     end
+-- )
